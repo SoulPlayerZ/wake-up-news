2f77aab67168d0e877941677e6902b6e40c4f830
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-01-31 20:39:52 -0300

    feat: Includes social media and logo test in Header. Without css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-
+import { FaTwitch, FaInstagram, FaGithub } from 'react-icons/fa';
+import { AiFillLinkedin } from 'react-icons/ai';
 
 function Header () {
    return(
      <header>
-        Header Component TESTE MAIN PAGE
+        <section>
+          <h2>LOGO TESTE</h2>
+        </section>
+        
+        <a href="https://github.com/SoulPlayerZ" target="_blank" rel="noreferrer noopener">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/wellington-fonseca-52032b1a4/" target="_blank" rel="noreferrer noopener">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://www.instagram.com/thyrsokof/?theme=dark" target="_blank" rel="noreferrer noopener">
+          <FaInstagram />
+        </a>
+        <a href="https://www.twitch.tv/soulplayer_z" target="_blank" rel="noreferrer noopener">
+          <FaTwitch />
+        </a>
      </header> 
    )
 }
