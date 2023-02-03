acdf2876890ba4722c0da5cc5c1c0f153d140fc4
SoulPlayerZ <thyrso.kof@gmail.com>
2023-02-03 14:40:12 -0300

    feat: Starts css from Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,29 @@
 import { FaTwitch, FaInstagram, FaGithub } from 'react-icons/fa';
 import { AiFillLinkedin } from 'react-icons/ai';
+import "../css/header.css";
 
 function Header () {
  
   return(
     <header>
-      <section>
-        <h2>LOGO TESTE</h2>
+      <section className="logo">
+        <h2>Wake Up News</h2>
       </section>
 
-      <a href="https://github.com/SoulPlayerZ" target="_blank" rel="noreferrer noopener">
-        <FaGithub />
-      </a>
-      <a href="https://www.linkedin.com/in/wellington-fonseca-52032b1a4/" target="_blank" rel="noreferrer noopener">
-        <AiFillLinkedin />
-      </a>
-      <a href="https://www.instagram.com/thyrsokof/?theme=dark" target="_blank" rel="noreferrer noopener">
-        <FaInstagram />
-      </a>
-      <a href="https://www.twitch.tv/soulplayer_z" target="_blank" rel="noreferrer noopener">
-        <FaTwitch />
-      </a>
+      <nav>
+        <a href="https://github.com/SoulPlayerZ" target="_blank" rel="noreferrer noopener" className='social-icon'>
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/wellington-fonseca-52032b1a4/" target="_blank" rel="noreferrer noopener" className='social-icon'>
+          <AiFillLinkedin />
+        </a>
+        <a href="https://www.instagram.com/thyrsokof/?theme=dark" target="_blank" rel="noreferrer noopener" className='social-icon'>
+          <FaInstagram />
+        </a>
+        <a href="https://www.twitch.tv/soulplayer_z" target="_blank" rel="noreferrer noopener" className='social-icon'>
+          <FaTwitch />
+        </a>
+      </nav>  
     </header> 
    )
 }
