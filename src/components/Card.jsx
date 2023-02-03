1c7f70139850bfc4bbd1296351ed7cb109417ca2
SoulPlayerZ <thyrso.kof@gmail.com>
2023-02-03 16:29:49 -0300

    feat: Start card format with css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,28 @@
+import "../css/card.css";
+
 function Card({ news }) {
 
-  return(
-    <a href={ news.url } target='_blank' rel="noreferrer noopener">
-        <h2>{ news.headline }</h2>
-        <p>{ news.inserted }</p>
-        <p>{ news.kicker }</p>
-        <img src={ `https://www.vagalume.com.br/${news.pic_src}`} alt='News Thumbnail' />
-    </a>
+  return( 
+    <section className="card">
+      <a className="card-content" href={ news.url } target='_blank' rel="noreferrer noopener">
+        <img className="card-img" src={ `https://www.vagalume.com.br/${news.pic_src}`} alt='News Thumbnail' />
+        <h2 className="card-title">{ news.headline }</h2>
+        <p className="card-title">{ news.kicker }</p>
+        <p className="card-title">{ news.inserted }</p>
+      </a>
+    </section>
   )
 }
 
 export default Card;
+
+
+// <section className="card">
+
+// <a  href={ news.url } target='_blank' rel="noreferrer noopener">
+//   <h2 className="card-title">{ news.headline }</h2>
+//   <img src={ `https://www.vagalume.com.br/${news.pic_src}`} alt='News Thumbnail' className="card-img-top" />
+//   <p className="card-kicker">{ news.kicker }</p>
+//   <p className="card-inserted">{ news.inserted }</p>
+// </a>
+// </section>
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,6 +1,8 @@
 import Card from "./Card";
 import { useContext, useEffect } from "react";
 import NewsContext from "../context/NewsContext";
+import "../css/cardList.css";
+
 
 function CardList () {
   const { allNews } = useContext(NewsContext);
@@ -14,7 +16,7 @@ function CardList () {
 
 
   return(
-    <section>
+    <section className="card-list">
         {allNews.map((news) => <Card news={news} key={ news.id }/>)}
     </section>
   )
