85e415bca959af6ddee032b26d5481682f265489
SoulPlayerZ <thyrso.kof@gmail.com>
2023-02-03 16:43:24 -0300

    Ainda alterando css dos cards. Commitando para terminar em casa

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,22 +7,11 @@ function Card({ news }) {
       <a className="card-content" href={ news.url } target='_blank' rel="noreferrer noopener">
         <img className="card-img" src={ `https://www.vagalume.com.br/${news.pic_src}`} alt='News Thumbnail' />
         <h2 className="card-title">{ news.headline }</h2>
-        <p className="card-title">{ news.kicker }</p>
-        <p className="card-title">{ news.inserted }</p>
+        <p className="card-kicker">{ news.kicker }</p>
+        <p className="card-inserted">{ news.inserted }</p>
       </a>
     </section>
   )
 }
 
 export default Card;
-
-
-// <section className="card">
-
-// <a  href={ news.url } target='_blank' rel="noreferrer noopener">
-//   <h2 className="card-title">{ news.headline }</h2>
-//   <img src={ `https://www.vagalume.com.br/${news.pic_src}`} alt='News Thumbnail' className="card-img-top" />
-//   <p className="card-kicker">{ news.kicker }</p>
-//   <p className="card-inserted">{ news.inserted }</p>
-// </a>
-// </section>
